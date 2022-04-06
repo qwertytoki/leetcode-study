e2a30598e302bddef72d9a6007f33355b51b43a5
qwertytoki <qwertytoki@gmail.com>
2022-04-07 06:51:46 +0800

    passed, still not good

---
+++ Array/1089_Duplicate_Zeros.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        for(int i =0; i<arr.length;i++){
+            if(arr[i]==0){
+                shiftArray(arr,i);
+                i++;
+            }
+        }
+    }
+    
+    private int[] shiftArray(int[]arr, int i){
+        for(int j = arr.length -1; j > i; j--){
+            arr[j] = arr[j-1];
+        }
+        return arr;
+    }
+}
