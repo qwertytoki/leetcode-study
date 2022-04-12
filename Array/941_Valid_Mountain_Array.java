70971f741a3125d039e10c49ad56bd3a905dad1b
qwertytoki <qwertytoki@gmail.com>
2022-04-13 07:33:19 +0800

    941 done

---
+++ Array/941_Valid_Mountain_Array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length <3){
+            return false;
+        }
+        int topIdx = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[topIdx]<arr[i]){
+                topIdx= i;
+            }
+        }
+        if(topIdx == 0 || topIdx == arr.length-1){
+            return false;
+        }
+        for(int i=1;i<topIdx;i++){
+            if(arr[i]==arr[topIdx]||arr[i]<=arr[i-1]){
+                return false;
+            }
+        }
+        for(int i=topIdx+1;i<arr.length;i++){
+            if(arr[i]==arr[topIdx]||arr[i]>=arr[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
