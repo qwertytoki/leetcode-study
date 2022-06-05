9743aab690adbff5e98953d51b241f63e861a3b5
qwertytoki <qwertytoki@gmail.com>
2022-06-05 11:34:15 +0800

    add comnment

--- LinkedList/82. Remove Duplicates from Sorted List I_Fail.java
+++ LinkedList/82. Remove Duplicates from Sorted List I_Fail.java
@@ -10,21 +10,25 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
+        // headの前に偽のNodeを用意する
         ListNode sentinel = new ListNode(0, head);
         ListNode pred = sentinel;
         
         while(head != null){
+//             今のValと次のValが同じだったら
             if(head.next != null && head.val == head.next.val){
+//                  同じの間だけ次へと進ませ続ける
                 while(head.next != null && head.val == head.next.val){
                     head = head.next;    
                 }
+//                 nextのValがちがうあたいになったので、そのNodeをpred.nextにいれて、次に進ませる
                 pred.next = head.next;    
-                
             } else{
                 pred = pred.next;
             }
             head = head.next;
         }
+//         作成した偽Headの次が本当のHeadなので、それを返してあげる
         return sentinel.next;
     }
 }
