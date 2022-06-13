37a0f69c65b2b5e1da1ed0a110e51787fa0594b4
qwertytoki <qwertytoki@gmail.com>
2022-06-13 22:54:21 +0800

    703 fail

---
+++ Heap/703. Kth Largest Element in a Stream_FAIL.java
@@ -0,0 +1,20 @@
+class KthLargest {
+
+    public KthLargest(int k, int[] nums) {
+        int[] nums;
+        int k;
+        this.nums = nums;
+        this.k = k;
+    }
+    
+    public int add(int val) {
+        nums[nums.length] = k;
+        
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
