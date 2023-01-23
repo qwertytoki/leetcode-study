5521d8dd063890b89dd83902de686f9f944fd684
qwertytoki <qwertytoki@gmail.com>
2023-01-23 15:14:21 +0800

    50. Pow(x, n) recursion

---
+++ Recursion/50. Pow(x, n) copy.java
@@ -0,0 +1,23 @@
+class Solution {
+    // recursion
+    private double fastPower(double x, long n) {
+        if(n == 0)return 1.0;
+
+        double half = fastPower(x, n/2);
+        
+        if(n%2==0){
+            return half * half;
+        }else{
+            return half * half * x;
+        }
+    }
+
+    public double myPow(double x, int n){
+        long nLong = n;
+        if(nLong<0){
+            x = 1/x;
+            nLong = -nLong;
+        }
+        return fastPower(x, nLong);
+    }
+}
