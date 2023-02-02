ccc864784e45822ab4950679e3352d00098b9bc2
qwertytoki <qwertytoki@gmail.com>
2023-02-03 02:13:27 +0800

    695

--- DFS,BFS/695. Max Area of Island_DFS_iterative.java
+++ DFS,BFS/695. Max Area of Island_DFS_iterative.java
@@ -1,26 +1,21 @@
-// something wrong
-
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         // DFS iterative way
         if(grid == null || grid.length == 0){
             return 0;
         }
-
         boolean[][] seen = new boolean[grid.length][grid[0].length];
         int[] rChange = new int[]{1,-1,0,0};
         int[] cChange = new int[]{0,0,1,-1};
-        
-
         int max = 0;
 
         for(int r =0; r<grid.length; r++){
             for(int c = 0;c<grid[0].length;c++){
                 if(grid[r][c] == 1 || !seen[r][c]){
                     int count = 0;
-                    seen[r][c] = true;
                     Stack<int[]> stack = new Stack<>();
                     stack.push(new int[]{r,c});
+                    seen[r][c] = true;
                     while(!stack.empty()){
                         int[] current = stack.pop();
                         count++;
@@ -29,8 +24,8 @@ class Solution {
                             int newC = current[1] + cChange[i];
                             if(newR>=0 && newC >= 0 && newR<grid.length && newC < grid[0].length 
                             && grid[newR][newC] == 1 && !seen[newR][newC]){
-                                seen[newR][newC] = true;
                                 stack.push(new int[]{newR,newC});
+                                seen[newR][newC] = true;
                             }
                         }
                     }
