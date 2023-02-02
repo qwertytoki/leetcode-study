ecd840d7226167411bbb8d8901f879f5898ad4ba
qwertytoki <qwertytoki@gmail.com>
2023-02-03 02:03:03 +0800

    695something wrong

--- DFS,BFS/695. Max Area of Island_DFS_iterative.java
+++ DFS,BFS/695. Max Area of Island_DFS_iterative.java
@@ -1,34 +1,44 @@
+// something wrong
+
 class Solution {
-    
-    int[][] grid;
-    boolean [][] seen; //こうやれば値を変えずに判定ができるのか
-    
     public int maxAreaOfIsland(int[][] grid) {
-        if(grid == null || grid.length==0){
+        // DFS iterative way
+        if(grid == null || grid.length == 0){
             return 0;
         }
+
+        boolean[][] seen = new boolean[grid.length][grid[0].length];
+        int[] rChange = new int[]{1,-1,0,0};
+        int[] cChange = new int[]{0,0,1,-1};
         
-        this.grid = grid;
-        seen = new boolean[grid.length][grid[0].length];
-        
-        int maxArea =0;
-        for(int r = 0;r<grid.length;r++){
-            for(int c = 0; c <grid[0].length; c++){
-                //現在の島の面積を探すロジック
-                int currArea = countArea(r,c);
-                maxArea= currArea > maxArea ? currArea : maxArea;
+
+        int max = 0;
+
+        for(int r =0; r<grid.length; r++){
+            for(int c = 0;c<grid[0].length;c++){
+                if(grid[r][c] == 1 || !seen[r][c]){
+                    int count = 0;
+                    seen[r][c] = true;
+                    Stack<int[]> stack = new Stack<>();
+                    stack.push(new int[]{r,c});
+                    while(!stack.empty()){
+                        int[] current = stack.pop();
+                        count++;
+                        for(int i =0;i<4;i++){
+                            int newR = current[0] + rChange[i];
+                            int newC = current[1] + cChange[i];
+                            if(newR>=0 && newC >= 0 && newR<grid.length && newC < grid[0].length 
+                            && grid[newR][newC] == 1 && !seen[newR][newC]){
+                                seen[newR][newC] = true;
+                                stack.push(new int[]{newR,newC});
+                            }
+                        }
+                    }
+                    max = Math.max(count,max);
+                }
             }
         }
-        return maxArea;
-    }
-    
-    private int countArea(int row, int col){
-        if(row<0 || col<0 || row>= grid.length || col>= grid[0].length || seen[row][col] || grid[row][col] ==0){
-            return 0;
-        }
-        seen[row][col] = true;
-        
-        return(1 + countArea(row-1,col)+ countArea(row+1,col)+ countArea(row,col-1)+ countArea(row,col+1));
-        
+
+        return max;
     }
 }
