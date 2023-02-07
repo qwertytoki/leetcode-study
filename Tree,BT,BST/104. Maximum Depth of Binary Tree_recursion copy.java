3316eb2f02e2e9f5439964b895c7ffd3b199c757
qwertytoki <qwertytoki@gmail.com>
2023-02-07 17:50:18 +0800

    aa

---
+++ Tree,BT,BST/104. Maximum Depth of Binary Tree_recursion copy.java
@@ -0,0 +1,12 @@
+//こうはならんやろ、、、、 (なってるやろがい)
+class Solution {
+    public int maxDepth(TreeNode root) {
+        // solve by recursion
+        if(root == null){
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth,rightDepth)+1;
+    }
+}
