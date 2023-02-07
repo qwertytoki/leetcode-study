ceea451da25b769a388be15687f4e67ea28c0764
qwertytoki <qwertytoki@gmail.com>
2023-02-08 00:22:09 +0800

    111 shorter way

---
+++ Tree,BT,BST/111. Minimum Depth of Binary Tree_DFS copy 2.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        //recursion
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null && root.right == null){
+            return 1;
+        }
+        int minDepth = Integer.MAX_VALUE;
+        
+        if(root.left != null){
+            minDepth = Math.min(minDepth(root.left) + 1, minDepth);
+        }
+        if(root.right != null){
+            minDepth = Math.min(minDepth(root.right) + 1, minDepth);
+        }
+        return minDepth;
+    }
+}
