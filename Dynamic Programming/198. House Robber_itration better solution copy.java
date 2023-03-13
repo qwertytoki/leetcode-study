f030f1123781bd04fd8ea6225e0fd58b30a43c2b
qwertytoki <qwertytoki@gmail.com>
2023-03-13 23:52:18 +0800

    198

---
+++ Dynamic Programming/198. House Robber_itration better solution copy.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        int t1 = 0;
+        int t2 = 0;
+        for(int i = 0; i< nums.length; i++){
+            int current = Math.max(t1, t2 + nums[i]);
+            t2 = t1;
+            t1 = current;
+        }
+        return t1;
+
+    }
+}
