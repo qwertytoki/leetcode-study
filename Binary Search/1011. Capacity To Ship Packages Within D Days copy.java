5279c489d83bd247e3bf1a80a42f12b0dd9625f0
qwertytoki <qwertytoki@gmail.com>
2023-03-27 02:49:34 +0800

    1011 add comment

--- Binary Search/1011. Capacity To Ship Packages Within D Days copy.java
+++ Binary Search/1011. Capacity To Ship Packages Within D Days copy.java
@@ -1,7 +1,9 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
         //最高値と合計値のMidをとると積載可能日数が算出できる。
-        //なぜかはまだ理解できていない
+        //もし答えが最小値となる場合、それはweightsの中で一番大きい値である。
+        //もし答えが最大値となる場合、それはweightsのトータルである。
+        //上記最小と最大のバイナリサーチをしていき、一番適当な値をみつける
         int max = 0;
         int total = 0;
         for(int i:weights){
