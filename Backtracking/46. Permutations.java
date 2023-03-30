688277b545aca65b6564e0a794c0ccef312204a1
qwertytoki <qwertytoki@gmail.com>
2023-03-31 01:55:22 +0800

    46 study

---
+++ Backtracking/46. Permutations.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        //Backtracking is an algorithm for finding all solutions by exploring all potential candidates.
+        List<List<Integer>> output = new LinkedList();
+        List<Integer> numsList = new ArrayList<>();
+        for(int num:nums){
+            numsList.add(num);
+        }
+        int n = nums.length;
+        backtrack(n,numsList,output,0);
+        return output;
+    }
+
+    private void backtrack(int n, List<Integer> nums, List<List<Integer>> output,int first){
+        if(first == n){
+            output.add(new ArrayList<Integer>(nums));
+        }
+        for(int i= first; i<n;i++){
+            Collections.swap(nums,first,i);
+            backtrack(n,nums,output,first+1);
+            Collections.swap(nums,first,i);
+        }
+    }
+}
