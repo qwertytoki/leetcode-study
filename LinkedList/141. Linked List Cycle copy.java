bf7ec1938a209791394583ef31c0cc7fb8aa3834
qwertytoki <qwertytoki@gmail.com>
2023-04-21 14:45:31 +0800

    141

---
+++ LinkedList/141. Linked List Cycle copy.java
@@ -0,0 +1,17 @@
+
+public class Solution {
+    //use 2pointer, fast and slow 
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
