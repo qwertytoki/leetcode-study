11bea674a976e14f17ac87ce8b54c287fdf954b6
qwertytoki <qwertytoki@gmail.com>
2023-05-07 09:43:31 +0800

    111 BFS

---
+++ Tree,BT,BST/111. Minimum Depth of Binary Tree_BFS copy 2.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //QueueでだすことによってBFSになるのか。 StackでだすとDFSになる
+    //かつ、BFSでだすということは深さ判定にMath.minを使わなくて良くなる。一番最初に見つかったところが答えだから
+    public int minDepth(TreeNode root) {
+        LinkedList<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+        if(root == null){
+            return 0;
+        }
+        queue.add(new Pair(root, 1));
+        int current = 0;
+        while(! queue.isEmpty()){
+            Pair<TreeNode, Integer> pair = queue.poll();
+            root = pair.getKey();
+            current = pair.getValue();
+            if(root.left == null && root.right == null){
+                break;
+            }
+            if(root.left != null){
+                queue.add(new Pair(root.left,current+1));
+            }
+            if(root.right != null){
+                queue.add(new Pair(root.right, current+1));
+            }
+
+        }
+        return current;
+    }
+}
