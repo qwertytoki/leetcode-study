9b964768153cec73418af69f3b53020ba8ad230f
qwertytoki <qwertytoki@gmail.com>
2023-06-21 15:48:37 +0100

    108

---
+++ Tree,BT,BST/108. Convert Sorted Array to Binary Search Tree copy 3.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    int[] nums;
+    public TreeNode sortedArrayToBST(int[] nums) {
+        this.nums = nums;
+        return generateBST(0, nums.length-1);
+    }
+
+    private TreeNode generateBST(int left, int right){
+        if(left > right) return null;
+        int mid = (left + right) /2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = generateBST(left, mid-1);
+        node.right = generateBST(mid+1, right);
+        return node;
+    }
+}
