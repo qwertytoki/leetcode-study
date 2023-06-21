15f54c49c53ef7d821dbbbf125d4717e0529f2a0
qwertytoki <qwertytoki@gmail.com>
2023-06-21 18:20:22 +0100

    112 recursion

---
+++ Tree,BT,BST/112. Path Sum copy 2.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null){
+            return false;
+        }
+        targetSum -= root.val;
+        if(targetSum == 0 && root.left == null && root.right == null){
+            return true;
+        }
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+    }
+}
