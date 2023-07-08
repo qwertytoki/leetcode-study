795002c0385a0ee5a58d5b6a00715f12cd000348
qwertytoki <qwertytoki@gmail.com>
2023-07-08 23:39:42 +0800

    90

---
+++ Backtracking/90. Subsets II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> subsets = new ArrayList<>();
+        List<Integer> currentSubset = new ArrayList<>();
+        subsetsWithDupHeader(subsets, currentSubset, nums, 0);
+        return subsets;
+    }
+
+    private void subsetsWithDupHeader(List<List<Integer>> subsets, List<Integer> currentSubset, int[] nums, int index){
+        subsets.add(new ArrayList<>(currentSubset));
+        for(int i = index; i < nums.length; i++){
+            if(i != index && nums[i] == nums[i-1]){
+                continue;
+            }
+            currentSubset.add(nums[i]);
+            subsetsWithDupHeader(subsets, currentSubset, nums, i+1);
+            currentSubset.remove(currentSubset.size() -1);
+        }
+    }
+}
