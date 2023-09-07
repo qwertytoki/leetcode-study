4f4e8ffd23b88602144bf83d7795dd6430a7d641
qwertytoki <qwertytoki@gmail.com>
2023-09-08 00:20:08 +0800

    700

--- Tree,BT,BST/700. Search in a Binary Search Tree_recursion.java
+++ Tree,BT,BST/700. Search in a Binary Search Tree_recursion.java
@@ -1,6 +1,14 @@
+
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root == null || val == root.val) return root;
-        return val < root.val ? searchBST(root.left, val): searchBST(root.right, val);
+        if(root == null || val == root.val){
+            return root;
+        }
+        if(root.val < val){
+            return searchBST(root.right, val);
+        }else{
+            return searchBST(root.left, val);
+        }
+        
     }
 }
