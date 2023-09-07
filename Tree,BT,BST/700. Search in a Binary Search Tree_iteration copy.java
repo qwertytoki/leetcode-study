bb8561185d43080bf4c024354d6d04b00177d222
qwertytoki <qwertytoki@gmail.com>
2023-09-08 00:23:52 +0800

    700

--- Tree,BT,BST/700. Search in a Binary Search Tree_iteration copy.java
+++ Tree,BT,BST/700. Search in a Binary Search Tree_iteration copy.java
@@ -1,10 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null || root.val == val){
+            return root;
+        }
         while(root != null && root.val != val){
-            if(val < root.val){
-                root = root.left;
-            }else{
+            if(root.val < val){
                 root = root.right;
+            }else{
+                root = root.left;
             }
         }
         return root;
