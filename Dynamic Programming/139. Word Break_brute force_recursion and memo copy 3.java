51636e06ab16a4310dc598c902591da3547761d6
qwertytoki <qwertytoki@gmail.com>
2023-10-29 12:19:16 +0800

    139

---
+++ Dynamic Programming/139. Word Break_brute force_recursion and memo copy 3.java
@@ -0,0 +1,22 @@
+class Solution {
+    Boolean[] memo;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        this.memo = new Boolean[s.length()+1];
+        return wordBreakHelper(s, new HashSet<>(wordDict), 0);
+    }
+
+    private boolean wordBreakHelper(String s, HashSet<String> set, int start){
+        if(start == s.length()){
+            return true;
+        }
+        if(memo[start] != null){
+            return memo[start];
+        }
+        for(int end = start; end <= s.length(); end++){
+            if(set.contains(s.substring(start, end)) && wordBreakHelper(s, set, end)){
+                return memo[start] = true;
+            }
+        }
+        return memo[start] = false;
+    }
+}
