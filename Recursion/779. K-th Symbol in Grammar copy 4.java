a8e7d3aa62183fe5866dd81a97d84e5840b91691
qwertytoki <qwertytoki@gmail.com>
2023-11-15 00:48:01 +0800

    779

---
+++ Recursion/779. K-th Symbol in Grammar copy 4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if(n==1 || k==1){
+            return 0;
+        }
+        int parent = kthGrammar(n-1, (k+1)/2 );
+        if(parent == 0){
+            if(k%2==0){
+                return 1;
+            }else{
+                return 0;
+            }
+        }else{
+            if(k%2==0){
+                return 0;
+            }else{
+                return 1;
+            }
+        }
+    }
+}
