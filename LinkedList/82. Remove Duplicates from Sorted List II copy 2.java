dcb9c82f560f57b46066ffdf58198b1c324532d3
qwertytoki <qwertytoki@gmail.com>
2023-12-29 00:39:39 +0800

    82

---
+++ LinkedList/82. Remove Duplicates from Sorted List II copy 2.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode sentinel = new ListNode(0,head);
+        ListNode predecessor = sentinel;
+        while(head != null){
+            if(head.next != null && head.val == head.next.val){
+                while(head.next != null && head.val == head.next.val){
+                    head = head.next;
+                }
+                predecessor.next = head.next;
+            }else{
+                predecessor = predecessor.next;
+            }
+            head = head.next;
+
+        }
+        return sentinel.next;
+    }
+}
