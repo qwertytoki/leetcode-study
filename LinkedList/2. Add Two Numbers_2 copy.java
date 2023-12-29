a9cc86a6d26abfc9678779ae31de4b2d6a4c3c39
qwertytoki <qwertytoki@gmail.com>
2023-12-30 01:48:08 +0800

    2

---
+++ LinkedList/2. Add Two Numbers_2 copy.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode sentinel = new ListNode(0);
+        ListNode current = sentinel;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0){ // carry != 0 これがキモ!
+            int i1 = l1 == null ? 0 : l1.val;
+            int i2 = l2 == null ? 0 : l2.val;
+            int sum = i1 + i2 + carry;
+            carry = sum /10;
+            current.next = new ListNode(sum%10);
+            current = current.next;
+            if(l1 != null){
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                l2 = l2.next;
+            }
+        }
+        return sentinel.next;
+    }
+}
