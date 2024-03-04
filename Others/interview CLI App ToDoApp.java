e7ce31f93a46501b6c086d19bb66da4cef60de38
qwertytoki <qwertytoki@gmail.com>
2024-03-04 16:51:05 +0800

    CLI app, todo app

--- Others/interview CLI App ToDoApp.java
+++ Others/interview CLI App ToDoApp.java
@@ -6,10 +6,6 @@ import lombok.*;
 public class ToDoItem{
     private String task;
     private boolean isCompleted;
-
-    public getItem(){
-        return (isCompleted: "[X]" ? "[]") + task;
-    }
 }
 
 
@@ -26,11 +22,44 @@ public class ToDoApp{
     }
 
     public void showAllTasks(){
-        return tasks;
+        if(tasks.isEmpty()){
+            System.out.println("There is no task now.");
+            return;
+        }
+        for(ToDoItem task:tasks){
+            System.out.println(task.getTask());
+        }
     }
 
     public static void main(String args[]){
-        
+        TodoApp app = new ToDoApp();
+        Scanner scanner = new Scanner();
+
+        while(true){
+            System.out.println("ToDo App");
+            System.out.println("1: Add task");
+            System.out.println("2: Show added tasks");
+            System.out.println("3: Exit");
+
+            int option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(option){
+                case 1:
+                    System.out.println("Please input a task.");
+                    String task = scanner.nextLine();
+                    app.addTask();
+                    break;
+                case 2:
+                    app.showAllTasks();
+                    break;
+                case 3:
+                    System.out.println("Exiting, bye.");
+                    return;
+                default:
+                    System.out.println("Please type option 1~3");
+            }
+        }
     }
 
 
