daba49b294b75d8c963f972942349a9131348df5
qwertytoki <qwertytoki@gmail.com>
2024-03-18 12:11:57 +0800

    49

---
+++ HashMap/49. Group Anagrams copy 6.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map = new HashMap<>();
+        for(String str : strs){
+            char[] c = str.toCharArray();
+            Arrays.sort(c);
+            String sortedStr = String.valueOf(c);
+            List strList = map.getOrDefault(sortedStr, new ArrayList<>());
+            strList.add(str);
+            map.put(sortedStr, strList);
+        }
+        return new ArrayList(map.values());
+    }
+}
