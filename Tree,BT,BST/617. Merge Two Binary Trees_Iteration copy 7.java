8ed0910abc8ee415ebeb7c0058d4692679f07668
qwertytoki <qwertytoki@gmail.com>
2024-04-09 01:55:49 +0800

    617

---
+++ Tree,BT,BST/617. Merge Two Binary Trees_Iteration copy 7.java
@@ -0,0 +1,32 @@
+
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 == null){
+            return root2;
+        }
+        
+        Stack<Pair<TreeNode, TreeNode>> stack = new Stack<>();
+        stack.add(new Pair(root1,root2));
+        while(!stack.isEmpty()){
+            Pair<TreeNode,TreeNode> pair = stack.pop();
+            TreeNode r1 = pair.getKey();
+            TreeNode r2 = pair.getValue();
+            if(r1== null || r2 == null){
+                continue;
+            }
+            r1.val += r2.val;
+            if(r1.left == null){
+                r1.left = r2.left;
+            }else{
+                stack.add(new Pair(r1.left,r2.left));
+            }
+
+            if(r1.right == null){
+                r1.right = r2.right;
+            }else{
+                stack.add(new Pair(r1.right, r2.right));
+            }   
+        }
+        return root1;
+    }
+}
