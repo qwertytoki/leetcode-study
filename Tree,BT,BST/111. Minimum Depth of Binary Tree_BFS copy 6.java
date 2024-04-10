7778663a14e6b639706d7606a2003aec59310861
qwertytoki <qwertytoki@gmail.com>
2024-04-10 21:37:45 +0800

    111

---
+++ Tree,BT,BST/111. Minimum Depth of Binary Tree_BFS copy 6.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int minDepth = Integer.MAX_VALUE;
+        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+        queue.add(new Pair(root, 1));
+
+        while(!queue.isEmpty()){
+            Pair<TreeNode,Integer> pair = queue.poll();
+            TreeNode node = pair.getKey();
+            int depth = pair.getValue();
+            if(node.left==null && node.right ==null){
+                return depth;
+            }
+            if(node.left != null){
+                queue.add(new Pair(node.left, depth+1));
+            }
+            if(node.right != null){
+                queue.add(new Pair(node.right, depth+1));
+            }
+        }
+
+        return minDepth;
+    }
+}
