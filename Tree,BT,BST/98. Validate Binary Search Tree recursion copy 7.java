c5312ea8fd7a45ec136e7f1923de7aac9f32c081
qwertytoki <qwertytoki@gmail.com>
2024-04-25 13:09:31 +0900

    98

---
+++ Tree,BT,BST/98. Validate Binary Search Tree recursion copy 7.java
@@ -0,0 +1,24 @@
+
+class Solution {
+
+    public boolean isValidBST(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        return validate(root, null, null);
+    }
+
+    private boolean validate(TreeNode node, Integer left, Integer right){
+        if(node == null){
+            return true;
+        }
+        if(left != null && node.val <= left){
+            return false;
+        }
+        if(right != null && node.val >= right){
+            return false;
+        }
+        
+        return validate(node.left, left, node.val) && validate(node.right, node.val, right);
+    }
+}
