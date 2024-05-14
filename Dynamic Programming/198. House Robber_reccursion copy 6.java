8dbe218af98f5f2d0f56cae47e1b805b4961067e
qwertytoki <qwertytoki@gmail.com>
2024-05-14 17:33:47 +0800

    198

--- Dynamic Programming/198. House Robber_reccursion copy 6.java
+++ Dynamic Programming/198. House Robber_reccursion copy 6.java
@@ -1,21 +1,22 @@
 class Solution {
+
     int[] memo;
     public int rob(int[] nums) {
-        memo = new int[100];
+        this.memo = new int[101];
         Arrays.fill(memo, -1);
         return helper(nums, 0);
     }
 
-    private int helper(int[] nums, int i){
-        if(i >= nums.length){
+    private int helper(int[] nums, int index){
+        if(index >= nums.length){
             return 0;
         }
-
-        if(memo[i] >= 0){
-            return memo[i];
+        if(memo[index]>-1){
+            return memo[index];
         }
-        memo[i] = Math.max(helper(nums, i+1), helper(nums, i+2)+nums[i]);
-        return memo[i];
-
+        memo[index] = Math.max(helper(nums, index+1), helper(nums, index+2) + nums[index]);
+        return memo[index];
     }
+
+    
 }
