08a47c682112fbba9f3f9264d118411f1b065aca
qwertytoki <qwertytoki@gmail.com>
2024-06-03 22:17:07 +0800

    102

---
+++ Tree,BT,BST/102. Binary Tree Level Order Traversal recursion copy 3.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, List<Integer>> map;
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        this.map = new HashMap<>();
+        if(root != null){
+            helper(root, 0);
+        }
+        
+        return new ArrayList(map.values());
+    }
+
+    private void helper(TreeNode root, int level){
+        
+        if(!map.containsKey(level)){
+            map.put(level, new ArrayList<>());
+        }
+        List<Integer> list = map.get(level);
+        list.add(root.val);
+        map.put(level, list);
+        if(root.left != null){
+            helper(root.left, level+1);
+        }
+        if(root.right != null){
+            helper(root.right, level+1);
+        }
+    }
+}
