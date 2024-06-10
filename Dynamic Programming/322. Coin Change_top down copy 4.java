a70b9ae48faaf4070a264c7b46500a9847eb67fd
qwertytoki <qwertytoki@gmail.com>
2024-06-10 18:04:30 +0800

    322

---
+++ Dynamic Programming/322. Coin Change_top down copy 4.java
@@ -0,0 +1,31 @@
+class Solutions {
+    int[] memo;
+
+    public int coinChange(int[] coins, int amount){
+        if(amount==0)return 0;
+        this.memo = new int[amount+1];
+        return changeHelper(coins, amount);
+    }
+
+    private int changeHelper(int[] coins, int amount){
+        if(amount == 0){
+            return 0;
+        }
+        if(amount < =){
+            return -1;
+        }
+        if(memo[amount] != 0){
+            return memo[amount];
+        }
+        int mid = Integer.MAX_VALUE;
+        for(int coin: coins){
+            int result = changeHelper(coins, amount - coin);
+            if(result >= 0 && result <min){
+                min = result +1;
+            }
+        }
+        memo[amount] = min==Integer.MAX_VALUE ? -1 : min;
+        return memo[amount];
+    }
+
+}
