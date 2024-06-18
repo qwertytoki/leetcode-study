908c01302d2868645cf316d0e5e81c92e084901a
qwertytoki <qwertytoki@gmail.com>
2024-06-18 22:06:58 +0800

    78

---
+++ Backtracking/78. Subsets backtracking copy 2.java
@@ -0,0 +1,26 @@
+//start endを操作しながら進んでいく
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int size = 0; size <= nums.length; size++) {
+            backtrack(nums, 0, size, result, new ArrayList<>());
+        }
+        return result;
+    }
+
+    private void backtrack(int[] nums, int start, int size, List<List<Integer>> result, List<Integer> current) {
+        if (current.size() == size) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        if (start >= nums.length) {
+            return;
+        }
+        for (int i = start; i < nums.length; i++) {
+            current.add(nums[i]);
+            backtrack(nums, i + 1, size, result, current);
+            current.remove(current.size() - 1);
+        }
+    }
+}
