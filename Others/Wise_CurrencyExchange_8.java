779aa29c1399280ffee25f516825fd4ca321276c
qwertytoki <qwertytoki@gmail.com>
2024-09-27 10:10:14 +0800

    commit the draft

---
+++ Others/Wise_CurrencyExchange_8.java
@@ -0,0 +1,31 @@
+
+class Wise_CurrencyExchange_8 {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class CurrencyConversionService {
+
+}
+
+interface CurrencyRateProvider {
+
+}
+
+class ProviderA implements CurrencyRateProvider {
+
+}
+
+class ProviderB implements CurrencyRateProvider {
+
+}
+
+class FallbackExchangeRateProvider implements CurrencyRateProvider {
+
+}
+
+class CacheManager {
+
+}
