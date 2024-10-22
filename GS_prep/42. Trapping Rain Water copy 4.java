5112969c10a3dcd8c16c16860b34c3adcde74c83
qwertytoki <qwertytoki@gmail.com>
2024-10-22 23:47:28 +0800

    42

---
+++ GS_prep/42. Trapping Rain Water copy 4.java
@@ -0,0 +1,23 @@
+
+class Solution {
+
+    public int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int ans = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = Math.max(leftMax, height[left]);
+                ans += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = Math.max(rightMax, height[right]);
+                ans += rightMax - height[right];
+                right--;
+            }
+        }
+        return ans;
+
+    }
+}
