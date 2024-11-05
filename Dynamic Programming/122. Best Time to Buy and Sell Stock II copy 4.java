7d44794f3de65433637a75964c354d577b3d17e0
qwertytoki <qwertytoki@gmail.com>
2024-11-06 02:58:59 +0800

    122

---
+++ Dynamic Programming/122. Best Time to Buy and Sell Stock II copy 4.java
@@ -0,0 +1,17 @@
+
+class Solution {
+
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int price = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (price < prices[i]) {
+                profit += prices[i] - price;
+                price = prices[i];
+            } else {
+                price = prices[i];
+            }
+        }
+        return profit;
+    }
+}
