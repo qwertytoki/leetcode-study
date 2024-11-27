e7ea491b219f0252444351c16add9a0a72b20b89
qwertytoki <qwertytoki@gmail.com>
2024-11-27 16:34:50 +0800

    125

---
+++ GS_prep/125. Valid Palindrome copy 2.java
@@ -0,0 +1,26 @@
+
+class Solution {
+
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        s = s.replace(" ", "");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(c);
+            }
+        }
+        s = sb.toString();
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
