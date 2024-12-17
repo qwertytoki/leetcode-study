8d4e63be8bf708d270a3077667dfa1310951ddcf
qwertytoki <qwertytoki@gmail.com>
2024-12-18 01:57:30 +0800

    134

---
+++ Array/134. Gas Station.java
@@ -0,0 +1,20 @@
+
+class Solution {
+
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0;
+        int tank = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            int netGas = gas[i] - cost[i];
+            total += netGas;
+            tank += netGas;
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        return total < 0 ? -1 : start;
+    }
+}
