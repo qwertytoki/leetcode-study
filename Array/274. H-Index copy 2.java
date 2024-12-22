92905efce5a85f3325ecec97d87231d8571d8d7d
qwertytoki <qwertytoki@gmail.com>
2024-12-23 01:49:28 +0800

    274

---
+++ Array/274. H-Index copy 2.java
@@ -0,0 +1,16 @@
+
+class Solution {
+
+    public int hIndex(int[] citations) {
+
+        Arrays.sort(citations);
+        int len = citations.length;
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] >= len) {
+                return len;
+            }
+            len--;
+        }
+        return 0;
+    }
+}
