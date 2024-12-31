daa0434097616d65469149c5c1404bebd039f3d7
qwertytoki <qwertytoki@gmail.com>
2024-12-31 16:21:34 +0800

    167

---
+++ HashMap/167. Two Sum II - Input Array Is Sorted copy.java
@@ -0,0 +1,14 @@
+
+class Solution {
+
+    public int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(numbers[i])) {
+                return new int[]{map.get(numbers[i]) + 1, i + 1};
+            }
+            map.put(target - numbers[i], i);
+        }
+        return new int[0];
+    }
+}
