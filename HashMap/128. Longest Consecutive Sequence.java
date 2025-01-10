f541665c3d3e79893add38f17d83726f9e129c57
qwertytoki <qwertytoki@gmail.com>
2025-01-11 02:10:33 +0800

    128

---
+++ HashMap/128. Longest Consecutive Sequence.java
@@ -0,0 +1,27 @@
+
+class Solution {
+
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int max = 0;
+
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+                while (set.contains(currentNum + 1)) {
+                    currentStreak++;
+                    currentNum++;
+                }
+                max = Math.max(max, currentStreak);
+            }
+        }
+        return max;
+    }
+}
